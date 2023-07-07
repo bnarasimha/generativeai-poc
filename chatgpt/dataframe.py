d162d067e3394b40bb6f71faf98330e02a755bff
Narasimha Badrinath <nbadrin1@in.ibm.com>
2023-07-07 16:16:19 +0530

    added survey results csv for next set of actions

--- chatgpt/dataframe.py
+++ chatgpt/dataframe.py
@@ -1,4 +1,4 @@
 import pandas as pd
 
-data =  pd.read_csv("/Users/narasimhabadrinath/Downloads/zOS_Participants_List.csv")
+data =  pd.read_csv("../assets/survey_results_public.csv")
 print(data.head())
